dc7b029d6e2a4aa106a90eabb87982814302ca6e
Ulaş Berke Yıldız <ulashjob@gmail.com>
2025-09-03 12:05:50 +0300

    .

--- backend/nginx/nginx_entry.sh
+++ backend/nginx/nginx_entry.sh
@@ -8,11 +8,13 @@ if [ ! -f /etc/nginx/ssl/localhost.crt ] || [ ! -f /etc/nginx/ssl/localhost.key 
     openssl genrsa -out /etc/nginx/ssl/localhost.key 2048
     
     # Generate certificate signing request and self-signed certificate
-    openssl req -new -x509 -key /etc/nginx/ssl/localhost.key -out /etc/nginx/ssl/localhost.crt -days 365 -subj "/C=TR/ST=Istanbul/L=Istanbul/O=Transcendence/OU=IT/CN=localhost.com"
+    openssl req -new -x509 -key /etc/nginx/ssl/localhost.key \
+        -out /etc/nginx/ssl/localhost.crt -days 365 \
+        -subj "/C=TR/ST=Istanbul/L=Istanbul/O=Transcendence/OU=IT/CN=localhost"
     
     # Set proper permissions
-    chmod 600 /etc/nginx/ssl/localhost.key
-    chmod 644 /etc/nginx/ssl/localhost.crt
+    chmod 755 /etc/nginx/ssl/localhost.key
+    chmod 755 /etc/nginx/ssl/localhost.crt
     
     echo "SSL certificates generated successfully!"
 else
@@ -24,5 +26,3 @@ echo "SSL configuration file created!"
 # Start nginx
 echo "Starting nginx..."
 exec "$@"
-echo "Starting nginx..."
-exec nginx -g "daemon off;"
