28d9d382bc66a3ce0ef37f50adb21f77eb89d6e3
Burak Demirbuken <bdemirbu@k2m01s03.42kocaeli.com.tr>
2025-10-16 13:30:04 +0300

    Update: Refactor player state retrieval in MultiplayerRoom to ensure accurate player data representation

--- backend/room/models/MultiPlayerRoom.js
+++ backend/room/models/MultiPlayerRoom.js
@@ -23,7 +23,7 @@ export default class MultiplayerRoom extends Room
 				1: [this.players[0]?.getState(this.host) || { id: null, name: "empty"}, this.players[2]?.getState(this.host) || { id: null, name: "empty"}],
 				2: [this.players[1]?.getState(this.host) || { id: null, name: "empty"}, this.players[3]?.getState(this.host) || { id: null, name: "empty"}],
 			},
-			players: this.players,
+			players: this.players.map(p => p.getState(this.host)),
 			spectators: this.spectators,
 			gameSettings: this.gameSettings,
 			createdAt: this.createdAt
