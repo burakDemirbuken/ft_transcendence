3d62f05e8b62be64735df341f80b2632192b6765
Nisa Ünnü <nisaunnu@gmail.com>
2025-11-14 11:40:38 +0300

    Profile.ts, dashboard.css: translations were edited, achievement section was parsed

--- frontend/src/Profile.ts
+++ frontend/src/Profile.ts
@@ -626,6 +626,7 @@ let profileManager: ManagerProfile;
 export function updateChartLanguage() {
 	if (profileManager)
 		profileManager.updateChartLanguage();
+	refreshMatchHistory();
 }
 
 function handleCardMouseMove(e: MouseEvent) {
@@ -1210,8 +1211,10 @@ async function populateRecentMatches(userName: string) {
 			matchItem.innerHTML = `
 				<div class="match-result">${isWin ? '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640"><!--!Font Awesome Free v7.1.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M320 576C461.4 576 576 461.4 576 320C576 178.6 461.4 64 320 64C178.6 64 64 178.6 64 320C64 461.4 178.6 576 320 576zM441 335C450.4 344.4 450.4 359.6 441 368.9C431.6 378.2 416.4 378.3 407.1 368.9L320.1 281.9L233.1 368.9C223.7 378.3 208.5 378.3 199.2 368.9C189.9 359.5 189.8 344.3 199.2 335L303 231C312.4 221.6 327.6 221.6 336.9 231L441 335z"/></svg>' : '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640"><!--!Font Awesome Free v7.1.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M320 64C178.6 64 64 178.6 64 320C64 461.4 178.6 576 320 576C461.4 576 576 461.4 576 320C576 178.6 461.4 64 320 64zM199 305C189.6 295.6 189.6 280.4 199 271.1C208.4 261.8 223.6 261.7 232.9 271.1L319.9 358.1L406.9 271.1C416.3 261.7 431.5 261.7 440.8 271.1C450.1 280.5 450.2 295.7 440.8 305L337 409C327.6 418.4 312.4 418.4 303.1 409L199 305z"/></svg>'}</div>
 				<div class="match-info">
-					<span class="opponent">${opponentName}</span>
-					<span class="score">${userScore}-${opponentScore}</span>
+					<div class="match-header">
+						<span class="opponent">&nbsp;${opponentName}</span>
+						<span class="score">${userScore}-${opponentScore}</span>
+					</div>
 				</div>
 			`;
 
@@ -1335,12 +1338,15 @@ async function setAchievementStats(user: any) {
 
 	achievements.forEach(card => {
 		const attrib = card.getAttribute("data-achievement");
-		if (user?.achievements[attrib]?.unlockedAt)
-		{
+		if (user?.achievements[attrib]?.unlockedAt) {
 			card.classList.replace("locked", "unlocked");
 			const date = card.querySelector(".achievement-date");
-			if (date)
-				date.textContent = user.achievements[attrib].unlockedAt;
+			if (date) {
+				// ✅ Tarihten sadece tarih kısmını al
+				const unlockedDate = new Date(user.achievements[attrib].unlockedAt);
+				const formattedDate = unlockedDate.toLocaleDateString('tr-TR'); // "14.11.2025" formatında
+				date.textContent = formattedDate;
+			}
 		}
 	});
 }
